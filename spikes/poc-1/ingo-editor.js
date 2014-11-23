b5a944ca9b1d6de64c96a32be5cfe402ad8c3f26
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-11-23 08:28:08 +0100

    extract board drawing into a service

--- spikes/poc-1/ingo-editor.js
+++ spikes/poc-1/ingo-editor.js
@@ -12,14 +12,33 @@
       {type: "triangle", position: "E1"},
     ];
   });
-  app.directive("goBoard", function() {
+  app.directive("goBoard", ["paper", "boardPainter", function(paper, painter) {
     return {
       link: function postLink(scope, iElement, iAttrs) {
         var canvas = iElement[0];
         paper.setup(canvas);
         var w = paper.view.viewSize.width, h = paper.view.viewSize.height;
-        var dw = w/19, dh = h/19;
-        var l = dw/2, r = w-dw/2, b = dh/2, t = h-dh/2;
+        painter.setup(w,h);
+        painter.drawLines();
+        paper.view.draw();
+      }
+    };
+  }]);
+  app.factory("paper", function() { return window.paper; });
+  app.factory("boardPainter", ["paper", function(paper) {
+    var w,h,dw,dh,l,r,b,t;
+    return {
+      setup: function(_w,_h) {
+        w = _w;
+        h = _h;
+        dw = w/19;
+        dh = h/19;
+        l = dw/2;
+        r = w-dw/2;
+        b = dh/2;
+        t = h-dh/2;
+      },
+      drawLines: function() {
         for(x=l; x<w; x+=dw) {
           var line = paper.Path.Line([x,b], [x,t]);
           line.strokeColor = "black";
@@ -28,8 +47,7 @@
           var line = paper.Path.Line([l,y], [r,y]);
           line.strokeColor = "black";
         }
-        paper.view.draw();
       }
     };
-  });
+  }]);
 })();
