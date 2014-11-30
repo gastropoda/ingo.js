813768caa69463aa37a0e2a38ddb6e8e12edfa61
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-11-30 11:34:08 +0100

    load and parse a game

--- ingo-editor.js
+++ ingo-editor.js
@@ -1,7 +1,7 @@
 (function() {
   angular
   .module("ingo-editor", ["go-board", "smartGame"])
-  .controller("BoardController", ["$scope", function($scope) {
+  .controller("BoardController", ["$scope", "$http", "SGF", function($scope, $http, SGF) {
     $scope.stones = [
       {color: "black", position: "C2"},
       {color: "black", position: "C3"},
@@ -12,5 +12,11 @@
       {type: "triangle", position: "D1"},
       {type: "triangle", position: "E1"},
     ];
+
+    var sgfPromise = $http.get("__go4go_20141129_Chen-Yiming_Chen-Si.sgf");
+    sgfPromise.success(function(rawSgf) {
+      var game = SGF.parse(rawSgf);
+      console.log(game);
+    });
   }]);
 })();
