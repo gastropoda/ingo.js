5e5645483eea30d1c210a2d17ba2d74272266ae2
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-12-01 22:24:56 +0100

    folder structure + karma setup

---
+++ karma.conf.js
@@ -0,0 +1,20 @@
+module.exports = function(config){
+  config.set({
+    basePath : "",
+    frameworks: ["mocha", "sinon-chai"],
+    files : [
+      "app/bower_components/angular/angular.js",
+      "app/js/**/*.js",
+      "spec/**/*_spec.js",
+    ],
+    autoWatch : true,
+    browsers : ["PhantomJS"],
+    reporters: ["mocha"],
+    plugins : [
+      "karma-phantomjs-launcher",
+      "karma-mocha",
+      "karma-mocha-reporter",
+      "karma-sinon-chai"
+    ],
+  });
+};
