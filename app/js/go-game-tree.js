442ef383036246c22ff03e74bcf2dd29dfdee9e8
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-12-01 23:13:45 +0100

    describe and implement an empty instance

--- app/js/go-game-tree.js
+++ app/js/go-game-tree.js
@@ -3,6 +3,12 @@
   .module("goGameTree", [])
   .factory("GoGameTree", function() {
     return function GoGameTree() {
+      this._parent = null;
+      this._state = null;
+      this._children = [];
+      this.parent = function() { return this._parent; }
+      this.state = function() { return this._state; }
+      this.children = function() { return this._children; }
     };
   });
 })();
--- spec/game_tree_spec.js
+++ spec/game_tree_spec.js
@@ -10,4 +10,22 @@ describe("GoGameTree", function() {
     var tree = new GoGameTree();
     expect(tree).to.be.instanceOf(GoGameTree);
   });
+
+  describe("fresh instance", function() {
+    beforeEach(function() {
+      this.tree = new GoGameTree();
+    });
+
+    it("has no parent", function() {
+      expect(this.tree.parent()).to.be.null;
+    });
+
+    it("has no state", function() {
+      expect(this.tree.state()).to.be.null;
+    });
+
+    it("has no children", function() {
+      expect(this.tree.children()).to.be.empty;
+    });
+  });
 });
