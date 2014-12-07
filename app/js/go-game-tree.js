19a0829ed93f2e710d1077bb12c09513f501e6dc
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-12-07 07:45:01 +0100

    change how prototype is written

--- app/js/go-game-tree.js
+++ app/js/go-game-tree.js
@@ -16,36 +16,41 @@
       }
     };
 
-    GoGameTree.prototype.parent = function() { return this._parent; }
-    GoGameTree.prototype.state = function() { return this._state; }
-    GoGameTree.prototype.children = function() { return this._children; }
-    GoGameTree.prototype.turnNumber = function() { return this._turnNumber; }
-    GoGameTree.prototype.move = function() { return this._move; }
-    GoGameTree.prototype.addChild = function(options) {
-      options = options || {};
-      if (options.move) {
-        var newState = this.state().deriveState(options.move);
-      }
-      var child = new GoGameTree({
-        parent: this,
-        state: newState,
-        move: options.move
-      });
-      this._children.push(child);
-      return child;
-    }
-    GoGameTree.prototype.findChild = function(move) {
-      var children = this.children();
-      for(var i in children) {
-        var child = children[i];
-        if (child.move() === move) {
-          return child;
+    GoGameTree.prototype = {
+      parent: function() { return this._parent; },
+      state: function() { return this._state; },
+      children: function() { return this._children; },
+      turnNumber: function() { return this._turnNumber; },
+      move: function() { return this._move; },
+
+      addChild: function(options) {
+        options = options || {};
+        if (options.move) {
+          var newState = this.state().deriveState(options.move);
         }
+        var child = new GoGameTree({
+          parent: this,
+          state: newState,
+          move: options.move
+        });
+        this._children.push(child);
+        return child;
+      },
+
+      findChild: function(move) {
+        var children = this.children();
+        for(var i in children) {
+          var child = children[i];
+          if (child.move() === move) {
+            return child;
+          }
+        }
+      },
+
+      findOrAddChild: function(move) {
+        return this.findChild(move) ||
+          this.addChild({move: move});
       }
-    }
-    GoGameTree.prototype.findOrAddChild = function(move) {
-      return this.findChild(move) ||
-        this.addChild({move: move});
     }
 
     return GoGameTree;
