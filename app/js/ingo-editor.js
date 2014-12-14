e60684d00dbd5f737f0bc2949023c4858c66b28a
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-12-14 12:03:29 +0100

    add next/prev buttons and their behavior

--- app/js/ingo-editor.js
+++ app/js/ingo-editor.js
@@ -1,11 +1,17 @@
 (function() {
   angular
-  .module("ingoEditor", ["goBoard", "goGameTree"])
+  .module("ingoEditor", ["goBoard", "goGameTree", "ui.bootstrap"])
   .controller("BoardController", ["$scope", "GoGameState", function($scope, GoGameState) {
     $scope.currentState = GoGameState.fromStrings(
       "..B",
       "..B",
       "...WW"
     );
+    $scope.nextGameState = function() {
+      console.log("next state");
+    };
+    $scope.prevGameState = function() {
+      console.log("previous state");
+    };
   }]);
 })();
