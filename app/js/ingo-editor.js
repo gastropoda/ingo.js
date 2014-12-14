77c459bfe4f9c42e6d2941d58b78c8e9b4e003a1
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-12-14 12:17:27 +0100

    emulate a game
    This is now broken because state's deriveState doesn't add the stone,
    as soon as that's implemented this should result in the original
    positon.
    
    I don't like how initial tree constructor needs two options, should make
    them defaults. Or make a special function for constructing a root node.

--- app/js/ingo-editor.js
+++ app/js/ingo-editor.js
@@ -1,17 +1,25 @@
 (function() {
   angular
   .module("ingoEditor", ["goBoard", "goGameTree", "ui.bootstrap"])
-  .controller("BoardController", ["$scope", "GoGameState", function($scope, GoGameState) {
-    $scope.currentState = GoGameState.fromStrings(
-      "..B",
-      "..B",
-      "...WW"
-    );
+  .controller("BoardController",
+              ["$scope", "GoGameState", "GoGameTree",
+                function($scope, GoGameState, GoGameTree) {
     $scope.nextGameState = function() {
       console.log("next state");
     };
     $scope.prevGameState = function() {
       console.log("previous state");
     };
+
+    var rootNode = new GoGameTree({
+      state: new GoGameState(),
+      turnNumber: 0
+    }), node = rootNode;
+    node = node.addChild({move: {black: "C2"}});
+    node = node.addChild({move: {white: "D1"}});
+    node = node.addChild({move: {black: "C3"}});
+    node = node.addChild({move: {white: "E1"}});
+
+    $scope.currentState = node.state();
   }]);
 })();
